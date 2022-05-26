3daf5dd0d816ba59004f4e3951a8fa72807d60f8
이상철 <zlrz6706@gmail.com>
2022-05-26 23:59:51 +0900

    자동 스크롤 로직

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 const navBar = document.querySelector('#navbar');
 const navBarHeight = navBar.getBoundingClientRect().height;
+const aboutBtn = document.querySelectorAll('.navbar__menu__item')[1];
 
 //스크롤 시 nav가 home화면보다 많이 내려갔을 경우 nav에 에니메이션 효과
 document.addEventListener('scroll', () => {
@@ -16,4 +17,18 @@ document.addEventListener('scroll', () => {
     navBar.classList.remove('navbar--dark')
   }
 })
+
+console.log("aboutBtn",aboutBtn)
+aboutBtn.addEventListener('click', goToTop)
+
+function goToTop() {
+  let topY = document.querySelector('#about').offsetTop - navBarHeight;
+  window.scrollTo({top:topY, behavior:"smooth"})
+}
+
+
+
+
+
+
 
